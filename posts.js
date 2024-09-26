71df976dc785d7d5bd4f99457eba4598e6d231b4
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-09-26 23:38:07 +0300

    kodların toparlanması

--- posts.js
+++ posts.js
@@ -1,7 +1,6 @@
 async function fetchUserPost() {
   const urlParams = new URLSearchParams(window.location.search);
-  const userId = urlParams.get("userId");
-  console.log(userId);
+  const userId = urlParams.get("id");
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
   );
@@ -17,25 +16,17 @@ async function fetchUserPost() {
 function userDatatoCard(userPost) {
   const postRow = document.getElementById("postRow");
   const div = document.createElement("div");
-  div.classList.add("col-lg-4");
   div.innerHTML = `
-        <div class="card">
-      <div class="card-body">
+      <div class="card">
+       <div class="card-body">
         <div class="card-text">Post Title :<span class="text-success">${userPost.title}</span></div>
-        <div class="card-text">Post Description : <span>${userPost.body}</span></div>
-      </div>
+      <div class="card-text">Post Description : <span>${userPost.body}</span></div>
+    </div>
     </div>
     `;
   postRow.appendChild(div);
 }
-function userIdInput() {
-  const ınputUserId = parseInt(prompt("Enter user id"));
-  if (ınputUserId < 1 || ınputUserId > 10) {
-    alert("Lütfen 1 ila 10 arasında sayı giriniz!!!");
-    userIdInput();
-  } else {
-    window.location.href = `posts.html?userId=${ınputUserId}`;
-  }
-}
-fetchUserPost();
-    
+
+window.onload = function () {
+  fetchUserPost();
+};
--- users.js
+++ users.js
@@ -1,84 +1,63 @@
 async function userGetFetchData() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const userData = await response.json();
-  userData.forEach((data) => {
-    userToCard(data);
-  });
-
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const userData = await response.json();
+    console.log(userData);
+    userToCard(userData);
+  } catch (error) {
+    console.error("ERROR Fetching User :" + error);
+  }
 }
-const cardContainerRow = document.getElementById("userCardRow");
-function userToCard(users) {
-  cardContainerRow.innerHTML += ` <div class="col-lg-3 mb-4  col-md-4 col-sm-6">
-          <div class="card text-black">
-            <div class="userInfo bg-body-secondary">
-              <div class="d-flex justify-content-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="36"
-                  height="36"
-                  fill="currentColor"
-                  class="bi bi-info-circle"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"
-                  />
-                  <path
-                    d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"
-                  />
-                </svg>
-              </div>
 
-                
-              <h4>Name:${users.name}</h4>
-              <h4>Username:${users.username}</h4>
-            </div>
-
-            <div class="userInfo bg-body-tertiary">
-              <div class="d-flex justify-content-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="36"
-                  height="36"
-                  fill="currentColor"
-                  class="bi bi-geo-alt"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"
-                  />
-                  <path
-                    d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"
-                  />
-                </svg>
-              </div>
-              <h5>Address:${users.address.street}</h5>
-              <h5>Address Suite ${users.address.suite}</h5>
-              <h5>Address City ${users.address.city}</h5>
-              <h5>Address ZipCode${users.address.zipcode}</h5>
-            </div>
-
-            <div class="userInfo">
-              <div class="d-flex justify-content-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="36"
-                  height="36"
-                  fill="currentColor"
-                  class="bi bi-person-lines-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z"
-                  />
-                </svg>
-              </div>
-              <h5>Mail:${users.email}</h5>
-              <h5>Phone:${users.phone}</h5>
-              <h5>WebSite:${users.website}</h5>
-              <h5>Company:${users.company.name}</h5>
-            </div>
+// RENDER USER CARTS
+const userToCard = (users) => {
+  const cardContainerRow = document.getElementById("userCardRow");
+  users.forEach((user) => {
+    const fetchUserCard = `
+      <div class="text-center d-flex mb-4">
+        <div class="card">
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title user-name"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+</svg>${user.name}</h5>
           </div>
-        </div>`;
-}
-userGetFetchData();
+          <div class="user-details">
+            <p class="card-text">${user.username}</p>
+            <p class="card-text"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+</svg>${user.address.city} & ${user.address.street}</p>
+            <p class="card-text"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+</svg>${user.email}</p>
+            <p class="card-text"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-telephone-inbound" viewBox="0 0 16 16">
+  <path d="M15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0m-12.2 1.182a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+</svg>${user.phone}</p>
+            <p class="card-text"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-building" viewBox="0 0 16 16">
+  <path d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5z"/>
+  <path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1zm11 0H3v14h3v-2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V15h3z"/>
+</svg>${user.website}</p>
+          </div>
+          <a href="posts.html?id=${user.id}" class="btn btn-dark">Details</a>
+        </div>
+      </div>
+    `;
+    cardContainerRow.innerHTML += fetchUserCard;
+  });
+};
+
+const searchForm = document.getElementById("searchInput");
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const searchValue = document.getElementById("searchInputValue").value;
+  const userId = parseInt(searchValue);
+  if (userId > 1 && userId <= 10) {
+    window.location.href = `posts.html?id=${userId}`;
+  } else {
+    alert("Hoopp Emmioglu Dikkat et");
+  }
+});
+
+window.onload = function () {
+  userGetFetchData();
+};
